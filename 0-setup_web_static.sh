d4d85dacbdb215b60aa360d978b8d9f0e6f34e66
lulu994 <loubna.neddah@gmail.com>
2023-10-05 12:16:38 +0200

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-# set up dummy html
-
-server="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
-file="/etc/nginx/sites-available/default"
-sudo apt-get update -y
-sudo apt-get install nginx -y
-sudo mkdir -p "/data/web_static/releases/test/"
-sudo mkdir "/data/web_static/shared/"
-echo "Holberton" > "/data/web_static/releases/test/index.html"
-rm -f "/data/web_static/current"; ln -s "/data/web_static/releases/test/" "/data/web_static/current"
-sudo chown -hR ubuntu:ubuntu "/data/"
-sudo sed -i "29i\ $server" "$file"
+# sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+printf "<html>\n\t<head></head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "/listen 80 default_server;/a location /hbnb_static {\\n\\talias /data/web_static/current/;\\n\\t}\\n" /etc/nginx/sites-available/default
 sudo service nginx restart
